933d9fec7571f4c5e9dc692744cc006c39a7c91a
Victor Acatrinei <Victor.Acatrinei@endava.com>
2019-06-04 19:47:06 +0300

    9/51

--- simple-shopping-cart-source/Scripts/app.js
+++ simple-shopping-cart-source/Scripts/app.js
@@ -1,5 +1,21 @@
-﻿$(document).ready(function() {
+﻿let currentList = {};
+
+function createShoppingList() {
+    currentList.name = $("#shoppingListName").val();
+
+    //app service call
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+}
+
+
+$(document).ready(function () {
     console.info("ready to go");
 });
+
 
 
