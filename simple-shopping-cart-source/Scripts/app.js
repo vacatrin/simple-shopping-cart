af6a3e2f8b3b378720b18f5c9a27e9e79bba1a1a
Victor Acatrinei <Victor.Acatrinei@endava.com>
2019-06-11 18:51:56 +0300

    added function to display shopping items.

--- simple-shopping-cart-source/Scripts/app.js
+++ simple-shopping-cart-source/Scripts/app.js
@@ -16,10 +16,23 @@ function createShoppingList() {
 function addItem() {
     let newItem = {};
     newItem.name = $("#newItemName").val();
-
     currentList.items.push(newItem);
-
     console.info(currentList);
+
+    drawItems();
+}
+
+function drawItems() {
+    let $list = $("#shoppingListItems").empty();
+
+    for (var i = 0; i < currentList.items.length; i++) {
+        var $li = $("<li>").html(currentList.items[i].name)
+            .attr("id", "item_" + i);
+        var $deleteBtn = $("<button>Del</button>").appendTo($li);
+        var $checkBtn = $("<button>Chk</button>").appendTo($li);
+
+        $li.appendTo($list);
+    }
 }
 
 $(document).ready(function () {
