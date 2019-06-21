70d7462a52d868e386aa2a656bbc03e80a030a62
Victor Acatrinei <Victor.Acatrinei@endava.com>
2019-06-21 15:51:07 +0300

    Sarted adding controllers

---
+++ simple-shopping-cart-source/Controllers/ShoppingListController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace simple_shopping_cart_source.Controllers
+{
+    public class ShoppingListController : ApiController
+    {
+        // GET: api/ShoppingList
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ShoppingList/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ShoppingList
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ShoppingList/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ShoppingList/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
