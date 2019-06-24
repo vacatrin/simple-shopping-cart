8f77b9ced306b797da65d913e71b54695f6f23c9
Victor Acatrinei <Victor.Acatrinei@endava.com>
2019-06-24 10:48:12 +0300

    added comment

--- simple-shopping-cart-source/Controllers/ShoppingListController.cs
+++ simple-shopping-cart-source/Controllers/ShoppingListController.cs
@@ -4,35 +4,43 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using simple_shopping_cart_source.Models;
 
 namespace simple_shopping_cart_source.Controllers
 {
     public class ShoppingListController : ApiController
     {
-        // GET: api/ShoppingList
-        public IEnumerable<string> Get()
+
+        // create a mock shopping list, before implementing the database
+        private List<ShoppingList> shoppingList = new List<ShoppingList>
         {
-            return new string[] { "value1", "value2" };
-        }
+            new ShoppingList()
+            {
+                Id = 0,
+                Name = "Groceries"
+            },
+            new ShoppingList()
+            {
+                Id = 1,
+                Name = "Tools"
+            }
+        };
 
         // GET: api/ShoppingList/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            ShoppingList result = shoppingList.FirstOrDefault(s => s.Id == id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
 
         // POST: api/ShoppingList
         public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/ShoppingList/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/ShoppingList/5
-        public void Delete(int id)
         {
         }
     }
